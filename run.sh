13bd78aa30b8aef68625476c9a8249b5f03e8b20
Michael Wyraz <michael@wyraz.de>
2018-01-06 21:04:47 +0100

    first commit

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -z "${PIPEWORK_WAIT_IF}" ]; then
+  chmod 700 /pipework
+  /pipework --wait -i ${PIPEWORK_WAIT_IF}
+fi
+
+echo "Creating user ${CUPS_ADMIN_USERNAME}"
+id ${CUPS_ADMIN_USERNAME} || adduser -H -S -G lpadmin ${CUPS_ADMIN_USERNAME}
+echo "${CUPS_ADMIN_USERNAME}:${CUPS_ADMIN_PASSWORD}" | chpasswd
+
+
+touch /var/log/cups/error_log
+chown root:lp /var/log/cups/error_log
+tail -f /var/log/cups/error_log &
+
+/usr/sbin/cupsd -f
